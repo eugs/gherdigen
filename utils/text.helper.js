fc186869a3978656626607ce95ec5401335205b4
eugs <>
2023-12-01 17:35:11 +0100

    Added insertTagTo function

--- utils/text.helper.js
+++ utils/text.helper.js
@@ -1,5 +1,5 @@
+const { getFeatureText} =  require('./fs.helper');
 const NAME_REGEX = /^(Scenario|Scenario Outline)(\s*|\t*)?\:(\s*|\t*)?/gim;
-
 
 function cleanUpName(name) {
 	name = name.trim();
@@ -7,6 +7,26 @@ function cleanUpName(name) {
 	return name;
 }
 
+function insertTagTo(scenarioName, tag, parentFeature) {
+
+	const featureText = getFeatureText(parentFeature);
+	const rows = featureText.split('\n');
+	const rowsFinal = rows.map((row) => {
+		if(row.match(scenarioName)) {
+			const match = row.match(/^(\s+|\t+)/);
+			const spaces = (match) ? match[0] : '';
+			return `${spaces}${tag}\n${row}`;
+			
+		} else {
+			return row;
+		}
+	});
+
+	const finalText = rowsFinal.join('\n');
+	return finalText;
+}
+
 module.exports = {
-	cleanUpName
+	cleanUpName,
+	insertTagTo
 }

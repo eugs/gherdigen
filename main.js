df660f5a2e90ef095aa9b0433371a6f05176be77
eugs <>
2023-12-02 03:58:30 +0100

    Msg to main

--- main.js
+++ main.js
@@ -4,23 +4,28 @@ const path = require('path');
 const { scanDir, getFeatureText, saveFeatureText, getConfig } = require('./utils/fs.helper');
 const { insertTagTo } = require('./utils/text.helper');
 const { generateID } = require('./utils/generator');
+const { EMPTY_SCENARIO, SCAN_FAILED, NO_FEATURES, INVALID_SCENARIOS_NUM } = require('./utils/colors/errors.map');
+const { UPDATE_MSG, STATUS_MSG, STATUS_ARG_MSG, TAG_ADDED }  = require('./utils/colors/messages.map');
 const config = getConfig();
 
 const FEATURES_DIR = config['featuresDir'] || './features';
 const FEATURES_PATH = path.resolve(__dirname, FEATURES_DIR);
+Error.stackTraceLimit = 0;
 
 function updateScenario(scenarioName) {
-	if (!scenarioName) throw new Error(`Expected scenario name but got '${scenarioName}'`);
+	if (!scenarioName) return console.log(EMPTY_SCENARIO(scenarioName));
+
 	const features = [];
 
 	try {
 		features.push(...scanDir(FEATURES_PATH));
 	} catch (err) {
-		console.error(`Error while scanning ${FEATURES_PATH}`);
+		console.log(SCAN_FAILED(FEATURES_PATH));
 	}
 
 	if (features.length === 0) {
-		throw new Error(`Haven't found any features in ${FEATURES_PATH}`);
+		console.log(NO_FEATURES(FEATURES_PATH));
+		return 1;
 	}
 
 	const parentFeature = [];
@@ -37,12 +42,13 @@ function updateScenario(scenarioName) {
 	});
 
 	if (parentFeature.length !== 1) {
-		throw new Error(`Found ${parentFeature.length} scenarios with name "${scenarioName}" in ${FEATURES_PATH}`);
+		// throw new Error(`Found ${parentFeature.length} scenarios with name "${scenarioName}" in ${FEATURES_PATH}`);
+		console.log(INVALID_SCENARIOS_NUM(parentFeature.length, scenarioName, FEATURES_PATH));
+		return 1;
 	}
 
 	const tag = generateID(scenarioName);
-
-	console.log(`\n...will try to update in file:\n${parentFeature[0]}`);
+	console.log(UPDATE_MSG(parentFeature[0]));
 
 	try {
 		const updatedText = insertTagTo(scenarioName, tag, parentFeature[0]);
@@ -51,10 +57,9 @@ function updateScenario(scenarioName) {
 		console.error(`scenario wasn't updated, some error occured\n${err.message}`);
 	}
 
-	console.log('====');
-	console.log('SUCESS!');
-	console.log('====');
-	console.log(`tag\t"${tag}"\nwas added to scenario:\t${scenarioName}`);
+
+	console.log(STATUS_ARG_MSG('', 'SUCCESS!'));
+	console.log(TAG_ADDED(tag, scenarioName));
 }
 
 module.exports = {
